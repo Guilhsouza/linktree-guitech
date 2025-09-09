82ec6f812b55c0c47e65735f0c20998b70169dcc
Guilherme Souza <gui.edu0380@gmail.com>
2025-09-08 22:15:14 -0300

    adicionando botões de redes sociais

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 'use client'
 import Image from "next/image";
 import logoNome from "./images/logo-nome.png"
+import wppLogo from "./images/whatsapp-logo.png"
+import facebookLogo from "./images/facebook-logo.png"
+import instaLogo from "./images/Instagram-logo.png"
 import CardBtn from "./CardBtn";
+
+import styles from "./styles.module.css"
 
 export default function Home() {
   return (
@@ -17,10 +22,37 @@ export default function Home() {
 
         <CardBtn content="WhatsApp" hrefDestiny="https://wa.me/5512991112772" />
         <CardBtn content="Instagram" hrefDestiny="https://www.instagram.com/guitech_assistencia/" />
-        <CardBtn content="Facebook" hrefDestiny="https://www.instagram.com/guitech_assistencia/" />
-        <CardBtn content="Site" hrefDestiny="https://www.instagram.com/guitech_assistencia/" />
+        <CardBtn content="Facebook" hrefDestiny="" />
+        <CardBtn content="Site" hrefDestiny="" />
       </div>
+      <div className="flex justify-center gap-14 mt-4">
+        <a href="https://wa.me/5512991112772" target="_blank">
+          <Image
+            alt="logo-whatsapp"
+            src={wppLogo}
+            width={35}
+            className={styles.imageScale}
+          />
+        </a>
 
-    </div>
+        <a href="https://www.instagram.com/guitech_assistencia/" target="_blank">
+          <Image
+            alt="logo-instagram"
+            src={instaLogo}
+            width={35}
+            className={styles.imageScale}
+          />
+        </a>
+
+        <a href="">
+          <Image
+            alt="logo-facebook"
+            src={facebookLogo}
+            width={33}
+            className={styles.imageScale}
+          />
+        </a>
+      </div>
+    </div >
   );
 }
